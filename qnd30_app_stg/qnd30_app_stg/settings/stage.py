a4c590e67fe285f30e28c0a8df7e71894ac596f4
smartquailprod <114889940+smartquailprod@users.noreply.github.com>
2023-05-10 10:17:04 -0500

    Update stage.py

--- qnd30_app_stg/qnd30_app_stg/settings/stage.py
+++ qnd30_app_stg/qnd30_app_stg/settings/stage.py
@@ -266,13 +266,15 @@ if DB_IS_AVIAL and POSTGRES_READY:
 
 #Static files DevMod
 
-STATIC_URL = "/static/"
-STATICFILES_DIRS = [BASE_DIR / "static"]  
-STATIC_ROOT = STATIC_ROOT = BASE_DIR / "staticfiles"
 STATICFILES_STORAGE = "django.contrib.staticfiles.storage.StaticFilesStorage"
+ALLOWED_HOSTS = ['*']
+
+#Static files DevMod
 
 MEDIA_URL = "/media/"
 MEDIA_ROOT  = os.path.join(BASE_DIR, 'media')
-
+STATICFILES_DIRS = [BASE_DIR / "staticfiles"]  
+STATIC_URL = "/static/"
+STATIC_ROOT = STATIC_ROOT = BASE_DIR / "static"
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
